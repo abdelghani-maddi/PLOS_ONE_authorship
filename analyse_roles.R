8b1eff603d09089d7f8885b58762896697fb80ff
Abdelghani Maddi <36671005+abdelghani-maddi@users.noreply.github.com>
2023-06-12 11:28:21 +0200

    suspect authors extraction

--- analyse_roles.R
+++ analyse_roles.R
@@ -16,17 +16,26 @@ library(readxl)
 ### Lecture des données ----
 
 row_data <- read_excel("~/Documents/APC Jaime Texiera/row_data.xlsx")
-df <- read_excel("~/Documents/APC Jaime Texiera/data_roles.xlsx")
+data_roles <- read_excel("~/Documents/APC Jaime Texiera/data_roles.xlsx")
 
+
+# Triez le dataframe par "DOI" et "authors_num" pour garantir l'ordre correct
+data_roles <- data_roles %>% arrange(DOI, authors_num)
+
+# Créez la nouvelle colonne "roles_corr" en utilisant la fonction lag() et ifelse()
+data_roles <- data_roles %>% 
+  group_by(DOI) %>% 
+  mutate(roles_corr = ifelse(authors_num == 1, "Writing – original draft", lag(Roles))) %>% 
+  ungroup()
 
 
 # Diviser les chaînes de caractères en vecteurs
-df <- df %>% 
-  separate_rows(Roles, sep = "\\s*,\\s*")
+df <- data_roles %>% 
+  separate_rows(roles_corr, sep = "\\s*,\\s*")
 
 # Grouper le dataframe par "authors" et "DOI" et compter le nombre de "Roles" pour chaque groupe
 result <- df %>%
-  select(authors, DOI, Roles) %>%
+  select(authors, DOI, roles_corr) %>%
   group_by(authors, DOI) %>%
   summarize(num_roles = n())
 
@@ -38,7 +47,7 @@ merged_df <- merge(df, result, by = c("DOI","authors"), all.x = TRUE)
 # suspects papers
 # Sélectionner les auteurs qui ont num_roles < 3 et "Funding acquisition"
 filtered_df <- merged_df %>%
-  filter(num_roles < 2, Roles == "Funding acquisition")
+  filter(num_roles < 2, roles_corr == "Funding acquisition")
 
 suspects_authors <- filtered_df %>%
   select(authors, DOI, num_roles)
