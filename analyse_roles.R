c3532849ffb75d532d1602c502ee5cd6492eeb3c
Abdelghani Maddi <maddiabdelghani@gmail.com>
2023-08-15 13:33:38 +0200

    data pays final df

--- analyse_roles.R
+++ analyse_roles.R
@@ -504,6 +504,57 @@ df_pays$doi <- gsub("^https://doi.org/", "", df_pays$doi)
 df_pays <- df_pays %>%
   filter(doi %in% doi_study$doi) 
 
+# Retravailler le df_pays pour extraire les multiaffilisation qui restent pour certains auteurs
+################################################################
+# lignes concrnées
+df_pays_pointvirgule <- df_pays %>%
+  filter(grepl(";", tolower(affiliations), ignore.case = TRUE))
+
+# Diviser la colonne "affiliations" en plusieurs lignes en utilisant "; " comme séparateur
+df_split_affiliations <- df_pays_pointvirgule %>%
+  separate_rows(affiliations, sep = "; ")
+
+ 
+
+# Extraire la dernière chaîne de caractères après la virgule ou le point-virgule de chaque élément de la colonne "affiliations"
+df_split_affiliations$pays <- sapply(strsplit(df_split_affiliations$affiliations, "[;,]\\s*"), function(x) tail(x, 1))
+
+# Supprimer les caractères "." et ";" à la fin de la colonne "affiliations"
+df_split_affiliations$pays <- gsub("[.;]+$", "", df_split_affiliations$pays)
+# Mettre en majuscule la première lettre de chaque mot dans la colonne "pays"
+df_split_affiliations$pays <- str_to_title(df_split_affiliations$pays)
+# Remplacer les motifs "United States Of America", par "USA" dans la colonne "pays"
+df_split_affiliations$pays <- gsub("United States Of America", "USA", df_split_affiliations$pays)
+# Remplacer les motifs "Republic Of Korea", par "Korea" dans la colonne "pays"
+df_split_affiliations$pays <- gsub("Republic Of Korea", "Korea", df_split_affiliations$pays)
+# Remplacer les motifs "Republic Of Korea", par "Korea" dans la colonne "pays"
+df_split_affiliations$pays <- gsub("Zoology Section Department Of Biology College Of Sciences Shiraz University Shiraz Iran", "Iran", df_split_affiliations$pays)
+# Remplacer les motifs "Republic Of Korea", par "Korea" dans la colonne "pays"
+df_split_affiliations$pays <- gsub("#N#", "", df_split_affiliations$pays)
+# Supprimer les caractères spéciaux dans la colonne "pays" du DataFrame "df"
+df_split_affiliations$pays <- gsub("[^[:alnum:] ]", "", df_split_affiliations$pays)
+
+# Harmoniser les pays ----
+# patterns <- read_excel("~/Documents/APC Jaime Texiera/patterns.xlsx")
+# rm(patterns)
+patterns <- read_excel("D:/APC Jaime T/patterns.xlsx")
+df_split_affiliations$pays_harmo <- NA
+# Parcourir le dataframe "patterns" pour remplacer les valeurs correspondantes dans "df_split_affiliations"
+for (i in 1:nrow(patterns)) {
+  # Récupérer le motif à rechercher et le remplacement associé
+  pattern <- patterns$patern[i]
+  replacement <- patterns$replace[i]
+  # Utiliser grepl pour vérifier si le motif est présent dans chaque valeur de "pays"
+  # Si c'est le cas, effectuer le remplacement avec le motif spécifié dans "replace"
+  df_split_affiliations$pays_harmo[grepl(pattern, df_split_affiliations$pays)] <- replacement
+}
+
+# Utiliser bind_rows pour faire l'union entre les deux DataFrames
+df_pays_fitre <- df_pays %>%
+  filter(!(grepl(";", tolower(affiliations), ignore.case = TRUE)))
+
+df_pays <- bind_rows(df_pays_fitre, df_split_affiliations)
+
 # Faire un left join
 # Au fait, j'avais éliminé dans OpenAlex les doi pour lesquels tous les auteus n'ont pas d'institution, ou inversement
 # mais la taille des deux df, celui extrait d'openalex et celui de plos laisse entendre que celui de plos contient plus
@@ -520,7 +571,30 @@ df_pays_misc <- df_pays %>%
   .[,4:12] %>%
   unique()
 # petite correction d'un vide constaté
-df_pays_misc$pays_harmo <- ifelse(df_pays_misc$pays=="Italia|Padova Italia", "Italy", df_pays_misc$pays_harmo)
+# Modifier la colonne pays_harmo en fonction de la condition pour distinguer entre Congo et RD de Congo
+df_pays_misc <- df_pays_misc %>%
+  mutate(pays_harmo = ifelse(
+    is.na(pays_harmo) & grepl("Italia|Padova Italia", pays, ignore.case = TRUE),
+    "Italy",
+    pays_harmo
+  ))
+
+# Modifier la colonne pays_harmo en fonction de la condition pour distinguer entre Congo et RD de Congo
+df_pays_misc <- df_pays_misc %>%
+  mutate(pays_harmo = ifelse(
+    pays_harmo == "Congo" & grepl("Democratic|Dr|Démocratique", pays, ignore.case = TRUE),
+    "Democratic Republic of the Congo",
+    pays_harmo
+  ))
+
+# Modifier la colonne pays_harmo en fonction de la condition pour distinguer entre Congo et RD de Congo
+df_pays_misc <- df_pays_misc %>%
+  mutate(pays_harmo = ifelse(
+    pays_harmo == "Dominica" & grepl("Dominica", pays, ignore.case = TRUE),
+    "Dominican Republic",
+    pays_harmo
+  ))
+
 # essayer de récupérer le max de pays_harmo à partir des noms de villes ou autre (à ce stade il y a 2.7% de NA)
 df_na_pays <- df_pays_misc %>%
   filter(is.na(pays_harmo))
@@ -530,17 +604,61 @@ df_na_pays <- df_pays_misc %>%
 # réimporter df_na_pays.xlsx après avoir fait un travail manuel de 
 # nettoyage et correction des données
 ####################################################################
-df_na_pays <- read_excel("D:/APC Jaime T/df_na_pays.xlsx")
+df_na_pays2 <- read_excel("D:/APC Jaime T/df_na_pays.xlsx")
+
+
+# Parcourir les lignes du df_na_pays
+for (i in 1:nrow(df_na_pays)) {
+  # Vérifier si le pays existe dans df_na_pays2
+  index <- match(df_na_pays$pays[i], df_na_pays2$pays)
+  
+  # Si le pays existe, mettre à jour pays_harmo
+  if (!is.na(index)) {
+    df_na_pays$pays_harmo[i] <- df_na_pays2$pays_harmo[index]
+  }
+}
+
+####
+patterns <- read_excel("D:/APC Jaime T/patterns.xlsx")
+
+# Parcourir le dataframe "patterns" pour remplacer les valeurs correspondantes dans "df_split_affiliations"
+for (i in 1:nrow(patterns)) {
+  # Récupérer le motif à rechercher et le remplacement associé
+  pattern <- patterns$patern[i]
+  replacement <- patterns$replace[i]
+  # Utiliser grepl pour vérifier si le motif est présent dans chaque valeur de "pays"
+  # Si c'est le cas, effectuer le remplacement avec le motif spécifié dans "replace"
+  df_na_pays$pays_harmo[grepl(pattern, df_na_pays$pays)] <- replacement
+}
+
 ##
 df_pays_misc_corr <-  df_pays_misc %>%
   filter(!(is.na(pays_harmo))) %>%
   rbind(., df_na_pays)
 # maintenant 99,1% des adresses sont nettoyées et corrigées (pays de l'institution)
-write.xlsx(df_pays_misc_corr, "D:/APC Jaime T/df_pays_misc_corr.xlsx" , all = TRUE)
+# write.xlsx(df_pays_misc_corr, "D:/APC Jaime T/df_pays_misc_corr.xlsx" , all = TRUE)
 
 # petite correction d'un vide constaté
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc$pays=="South Korea", "Korea", df_pays_misc$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays=="Korea", "South Korea", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Korea", "South Korea", df_pays_misc_corr$pays_harmo)
 
+####################################################################
+####################################################################
+#####################################################
+# pays qui ne matchent pas avec countryExData
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Maroc", "Morocco", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="USA", "United States" , df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Central African Republic", "Central African Republic", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Czech Republic", "Czech Rep.", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Dominica", "Dominican Rep.", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="North Macedonia", "Macedonia", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Vietnam", "Viet Nam", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="United Arab Emirates", "United Arab Emirates", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Korea", "South Korea", df_pays_misc_corr$pays_harmo)
+df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays=="Accra", "Ghana", df_pays_misc_corr$pays_harmo)
+
+
+####################################################################
 ####################################################################
 # compte fractionnaire
 df_pays_misc_corr <- df_pays_misc_corr %>%
@@ -560,7 +678,7 @@ count_typ_misc <- df_pays_misc_corr %>%
   group_by(pays_harmo, type_misc) %>%
   summarise(total = sum(frac)) %>%
   pivot_wider(names_from = type_misc, values_from = total, values_fill = 0)
-write.xlsx(count_typ_misc, "D:/APC Jaime T/count_typ_misc.xlsx" , all = TRUE)
+write.xlsx(count_typ_misc, "D:/APC Jaime T/count_typ_misc2.xlsx" , all = TRUE)
 
 
 # Parts
@@ -571,10 +689,10 @@ part_typ_misc <- df_pays_misc_corr %>%
   group_by(pays_harmo, type_misc) %>%
   summarise(part = sum(frac) / first(total_type_misc)) %>%
   pivot_wider(names_from = type_misc, values_from = part, values_fill = 0)
-write.xlsx(part_typ_misc, "D:/APC Jaime T/part_typ_misc.xlsx" , all = TRUE)
+write.xlsx(part_typ_misc, "D:/APC Jaime T/part_typ_misc2.xlsx" , all = TRUE)
 
 count_part_typ_misc <- merge(count_typ_misc, part_typ_misc, by = "pays_harmo")
-write.xlsx(count_part_typ_misc, "D:/APC Jaime T/count_part_typ_misc.xlsx" , all = TRUE)
+write.xlsx(count_part_typ_misc, "D:/APC Jaime T/count_part_typ_misc2.xlsx" , all = TRUE)
 
 ###
 
@@ -584,19 +702,9 @@ write.xlsx(count_part_typ_misc, "D:/APC Jaime T/count_part_typ_misc.xlsx" , all 
 require(rworldmap)
 data(countryExData)
 write.xlsx(countryExData, "D:/APC Jaime T/list_countries.xlsx" , all = TRUE)
-#####################################################
-# pays qui ne matchent pas avec countryExData
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Maroc", "Morocco", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="USA", "United States" , df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Central African Republic", "Central African Republic", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Czech Republic", "Czech Rep.", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Dominica", "Dominican Rep.", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="North Macedonia", "Macedonia", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Vietnam", "Viet Nam", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="United Arab Emirates", "United Arab Emirates", df_pays_misc_corr$pays_harmo)
-df_pays_misc_corr$pays_harmo <- ifelse(df_pays_misc_corr$pays_harmo=="Korea", "South Korea", df_pays_misc_corr$pays_harmo)
 
-# les scripts précédents sont donc relancés après cette correction.
+
+# les scripts précédents sur le comptage sont donc relancés après cette correction.
 #####################################################
 
 df_nb_part_countries <- left_join(count_part_typ_misc, countryExData, by = c("pays_harmo" = "Country"))
@@ -609,9 +717,11 @@ write.xlsx(df_nb_part_countries_na, "D:/APC Jaime T/df_nb_part_countries_na.xlsx
 # récupérer les infos géographiques de ces pays manquants
 # Créer un dataframe avec les informations
 data_pays_na <- data.frame(
-  pays_harmo = c("Afghanistan", "Andorra", "Bahrain", "Barbados", "Bhutan", "Brunei", "Cabo Verde", "Central African Republic", "Dominican Republic", "Eswatini", "Gambia", "Gibraltar", "Grenada", "Hong Kong", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Macao", "Maldives", "Malta", "Monaco", "Montenegro", "Qatar", "Réunion", "San Marino", "Serbia", "Seychelles", "Singapore", "Somalia", "Suriname", "The Bahamas", "United Arab Emirates"),
-  ISO3V10 = c("AFG", "AND", "BHR", "BRB", "BTN", "BRN", "CPV", "CAF", "DOM", "SWZ", "GMB", "GIB", "GRD", "HKG", "LSO", "LBR", "LBY", "LIE", "MAC", "MDV", "MLT", "MCO", "MNE", "QAT", "REU", "SMR", "SRB", "SYC", "SGP", "SOM", "SUR", "BHS", "ARE"),
-  EPI_regions = c("Asia & Pacific", "Europe", "Middle East", "Americas", "Asia & Pacific", "Asia & Pacific", "Africa", "Africa", "Americas", "Africa", "Africa", "Europe", "Americas", "Asia & Pacific", "Africa", "Africa", "Africa", "Europe", "Asia & Pacific", "Europe", "Europe", "Europe", "Europe", "Middle East", "Africa", "Europe", "Europe", "Africa", "Asia & Pacific", "Africa", "Americas", "Americas", "Middle East"))
+  pays_harmo = c("Afghanistan", "Andorra", "Bahrain", "Barbados", "Bhutan", "Bosnia And Hercegovina", "Brunei", "Cabo Verde", "Central African Republic", "Cote Divoire", "Dominican Republic", "Eswatini", "Gambia", "Gibraltar", "Grenada", "Hong Kong", "Lao Pdr", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Macao", "Maldives", "Malta", "Monaco", "Montenegro", "New Caledonia", "Palestine", "Puerto Rico", "Qatar", "Réunion", "San Marino", "Serbia", "Seychelles", "Singapore", "Somalia", "Suriname", "The Bahamas", "Trinidad And Tobago", "United Arab Emirates", "West Africa", "Democratic Republic of the Congo"),
+  ISO3V10 = c("AFG", "AND", "BHR", "BRB", "BTN", "BIH", "BRN", "CPV", "CAF", "CIV", "DOM", "SWZ", "GMB", "GIB", "GRD", "HKG", "LAO", "LSO", "LBR", "LBY", "LIE", "MAC", "MDV", "MLT", "MCO", "MNE", "NCL", "PSE", "PRI", "QAT", "REU", "SMR", "SRB", "SYC", "SGP", "SOM", "SUR", "BHS", "TTO", "ARE", "WAF", "COD"),
+  EPI_regions = c("Asia & Pacific", "Europe", "Middle East", "Americas", "Asia & Pacific", "Europe", "Asia & Pacific", "Africa", "Africa", "Africa", "Americas", "Africa", "Africa", "Europe", "Americas", "Asia & Pacific", "Asia & Pacific", "Africa", "Africa", "Africa", "Europe", "Asia & Pacific", "Asia & Pacific", "Europe", "Europe", "Europe", "Europe", "Middle East", "Americas", "Middle East", "Africa", "Europe", "Europe", "Africa", "Asia & Pacific", "Africa", "Americas", "Americas", "Middle East", "Africa", "Africa", "Sub-Saharan Africa")
+)
+write.xlsx(data_pays_na, "D:/APC Jaime T/data_pays_na.xlsx" , all = TRUE)
 
 # Mettre à jour les valeurs de df_nb_part_countries
 
@@ -624,5 +734,60 @@ for (i in 1:length(data_pays_na$pays_harmo)) {
   df_nb_part_countries$EPI_regions[df_nb_part_countries$pays_harmo == pays_harmo & is.na(df_nb_part_countries$EPI_regions)] <- data_pays_na$EPI_regions[i]
 }
 
-write.xlsx(df_nb_part_countries[,1:11], "D:/APC Jaime T/df_nb_part_countries.xlsx" , all = TRUE)
+write.xlsx(df_nb_part_countries[,1:11], "D:/APC Jaime T/df_nb_part_countries2.xlsx" , all = TRUE)
 
+###########################################################
+###########################################################
+# Pivoter les données pour obtenir les modalités de type_misc en colonnes
+pivot_data <- df_pays_misc_corr %>%
+  select(doi, type_misc) %>%
+  unique() %>%
+  mutate(value = 1) %>%
+  pivot_wider(names_from = type_misc, values_from = value, values_fill = 0) %>%
+  # .[,c(3:7)] %>%
+  # unique() %>%
+  group_by(doi) %>%
+  summarise(across(everything(), sum)) %>% # pour avoir sur la même lignes les différentes combinaisons 1 | 0 | 1 
+  group_by(across(-doi)) %>% # Calculer le nombre distinct de DOI par misc_type
+  summarise(count = n_distinct(doi)) 
+###########################################################
+###########################################################
+algeria <- df_pays_misc_corr %>% filter(pays_harmo=="Algeria") %>% select(frac)
+
+sum(algeria$frac)
+df_pays_algeria <- df_pays %>%
+     filter(grepl("algeria|algérie", affiliations, ignore.case = TRUE))
+df_pays_algeria <- df_pays %>%
+     filter(grepl("algeria|algérie", tolower(affiliations), ignore.case = TRUE))
+
+n_distinct(df_pays_algeria$doi)
+
+n_distinct(algeria$doi)
+
+setdiff(df_pays_algeria$doi, algeria$doi)
+
+sum(pivot_data$count)
+
+
+#######################################################################
+# > sum(pivot_data$count)
+# [1] 81823
+# > sum(df_nb_part_countries$`Authorship meets the criteria defined by PLOS One.x`)
+# [1] 81823
+# > sum(df_nb_part_countries$`Not meet authorship criteria.x`)
+# [1] 5448
+# > sum(df_nb_part_countries$`Authorship by resources.x`)
+# [1] 2593
+# > sum(df_nb_part_countries$`APC ring.x`)
+# [1] 35
+# > sum(df_nb_part_countries$`Authorship meets the criteria defined by PLOS One.y`)
+# [1] 1
+# > sum(df_nb_part_countries$`Not meet authorship criteria.y`)
+# [1] 1
+# > sum(df_nb_part_countries$`Not meet authorship criteria.y`)
+# [1] 1
+# > sum(df_nb_part_countries$`Authorship by resources.y`)
+# [1] 1
+# > sum(df_nb_part_countries$`APC ring.y`)
+# [1] 1
+####################################################################################
